7c7ccfa17f51a074ac56e0ac30fc1cd0b5c2b19e
vico958 <vico958@gmail.com>
2024-07-06 22:48:59 +0300

    add in services, user which will be my accessor

---
+++ User/server/services/user/userDatabaseMange.js
@@ -0,0 +1,76 @@
+const { user } = require("../../storages/models/user")
+
+class userDatabaseManage {
+    register = async (userToRegister) => {
+        const newUser = new user({
+            email:userToRegister.email,
+            password:userToRegister.password,
+            fullName:userToRegister.fullName,
+            preferences:userToRegister.preferences,
+            categories:userToRegister.categories
+        })
+        try{
+            const result = await newUser.save()
+            return result;
+        } catch(error){
+            console.log("shame", error)
+        }
+    }
+
+    getUserByEmail = async (email) => {
+        try{
+            return await user.findOne({email:email});
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    
+    getUserById = async (userId) => {
+        try{
+            return await user.findOne({_id:userId});
+        }catch(error){
+            console.log(error);
+        }
+    }
+    
+    changePassword = async (userId, newPassword) => {
+        try{
+            return await changeUserDataHelper(userId, newPassword, "password")
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    changeFullName = async (userId, newFullName) => {
+        try{
+            return await changeUserDataHelper(userId, newFullName, "fullName");
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    changeUserCategories = async (userId, newCategories) => {
+        try{
+            return await changeUserDataHelper(userId, newCategories, "categories")
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    changeUserPreferences = async (userId, newPreferences) => {
+        try{
+            return await changeUserDataHelper(userId, newPreferences, "preferences")
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    changeUserDataHelper = async(userId, newData, dataField) => {
+        return await user.findByIdAndUpdate({_id:userId}, {dataField: newData}, {new:true})
+
+    }
+}
+
+module.exports = userDatabaseManage
---
+++ User/server/services/user/userManger.js
@@ -0,0 +1,66 @@
+const userDatabaseManage = require("./userDatabaseMange")
+
+class userManger{
+    constructor(){
+        this.userDatabase = new userDatabaseManage();
+    }
+
+    async register (userToRegister) {
+        try{
+            return await this.userDatabase.register(userToRegister);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    async getUserByEmail (email) {
+        try{
+            return await this.userDatabase.getUserByEmail(email);
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    
+    async getUserById(userId) {
+        try{
+            return await this.userDatabase.getUserById(userId);
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+    async changePassword(userId, newPassword){
+        try{
+            return await this.userDatabase.changePassword(userId, newPassword)
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+    async changeFullName(userId, newFullName){
+        try{
+            return await this.userDatabase.changeFullName(userId, newFullName)
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+    async changeUserCategories(userId, newCategories){
+        try{
+            return await this.userDatabase.changeUserCategories(userId, newCategories);
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    async changeUserPreferences(userId, newPreferences){
+        try{
+            return await this.userDatabase.changeUserPreferences(userId, newPreferences);
+        }catch(error){
+            console.log(error);
+        }
+    }
+}
+
+module.exports = new userManger();
