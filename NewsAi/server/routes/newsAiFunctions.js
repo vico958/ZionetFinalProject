a702d981f988165718995b25cc00592f76def1ba
vico958 <vico958@gmail.com>
2024-07-09 00:52:06 +0300

    change variable to be inside a method

--- NewsAi/server/routes/newsAiFunctions.js
+++ NewsAi/server/routes/newsAiFunctions.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const aiApiKey = process.env.AI_API_KEY
 const genAI = new GoogleGenerativeAI(aiApiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json" }});
 
 async function whichOneIsTheBestArticle(req, res){
     try{
@@ -23,6 +22,7 @@ async function whichOneIsTheBestArticle(req, res){
 }
 
 async function talkWithAi(articlesInfo, preferences){
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json" }});
     const prompt ="take this link list make a summry of each article inside them and choose me the best 3 that will fit this preferences";
     const newPromt = `${prompt} ${preferences.toString()}`
     const result = await model.generateContent([newPromt, JSON.stringify(articlesInfo)])
