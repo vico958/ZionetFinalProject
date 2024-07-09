d3da3489d31a0914582ea1f82903c7b79aab6ff5
vico958 <vico958@gmail.com>
2024-07-09 17:05:04 +0300

    create 2 end points of chage Categories And Preferences

--- User/server/routes/user.js
+++ User/server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { userLogin, userRegister, changePassword, deleteUser } = require("./userFunctions")
+const { userLogin, userRegister, changePassword, deleteUser, chageCategoriesAndPreferences, chagePreferences } = require("./userFunctions")
 const userRouter = express.Router();
 
 userRouter.post("/register", userRegister);
-userRouter.post("/login", userLogin);
+userRouter.put("/change-categories-and-preferences", chageCategoriesAndPreferences);
+userRouter.put("/change-preferences", chagePreferences);
 userRouter.put("/change-password", changePassword)
 userRouter.delete("/delete-user", deleteUser)
 module.exports = userRouter;
