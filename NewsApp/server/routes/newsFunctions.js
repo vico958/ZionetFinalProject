ef13cf616fe089c1e6881819cf20d94e968b7b43
vico958 <vico958@gmail.com>
2024-07-09 13:11:41 +0300

    few changes to fix bugs

--- NewsApp/server/routes/newsFunctions.js
+++ NewsApp/server/routes/newsFunctions.js
@@ -12,14 +12,15 @@ async function getNews(req, res){
 async function userRegister(req, res){
     try{
         const { userToRegister} = req.body;
+        console.log(userToRegister);
         const returnedUser = await registerUserUsingAccessor(userToRegister)
         const {categories, preferences, fullName } = returnedUser;
         const message = `${fullName} you signed to the news app, we will send to you via email the news`
-        res.status(200).send(message);
+        res.status(200).send(returnedUser);
         res.end();
 
-        const news = await getNewsUsingEngine(categories).then(async (res) =>await res.json())
-        const bestNews = await bestFitNewsWithAi(news, preferences) // TODO: need to do that
+        // const news = await getNewsUsingEngine(categories).then(async (res) =>await res.json())
+        // const bestNews = await bestFitNewsWithAi(news, preferences) // TODO: need to do that
 
     }catch(error){
         console.log(error)
