77ef0d81a7beb6434cd8bc43fa8ad93a528e6b03
vico958 <vico958@gmail.com>
2024-07-09 22:10:14 +0300

    fix not working end point

--- NewsAi/server/routes/newsAiFunctions.js
+++ NewsAi/server/routes/newsAiFunctions.js
@@ -7,7 +7,7 @@ async function whichOneIsTheBestArticle(req, res){
     try{
         const preferences = req.body.preferences;
         const articles = req.body.articles;
-        const response = talkWithAi(articles, preferences);
+        const response = await talkWithAi(articles, preferences);
         res.status(200).send(JSON.stringify(response));
         res.end();
     }catch(error){
@@ -16,11 +16,21 @@ async function whichOneIsTheBestArticle(req, res){
 }
 
 async function talkWithAi(articlesInfo, preferences){
+    // TODO: make ai to give me results only about my preferences
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json" }});
-    const prompt ="take this link list make a summry of each article inside them and choose me the best 3 that will fit this preferences";
-    const newPromt = `${prompt} ${preferences.toString()}. as well return me the summry you made`
+    const prompt =`take this list, for each link in the list,
+    please enter to that and make a summry of each article inside them.
+    after that choose for me the best 3 articles that will fit this preferences`;
+    const newPromt = `${prompt} ${preferences.toString()} and return to me this 3 and only this 3.
+    when you return to me the 3 articles please return them with all the info.
+    please add the summry you did for each one of them with a field name summry.
+    in addition to that, if there is not enough articles to return because they dont talk about
+    the preferences i told you,
+    just return up to 3 that talk about this preferences, if its none of them its ok as well.
+    i dont want to see an article about food with preferences for lets say pizza and that you
+    return to me article that is about food but talk about ice cream`
     const result = await model.generateContent([newPromt, JSON.stringify(articlesInfo)])
-    const response = await result.response;
+    const response = result.response;
     const text = response.text();
     const jsonResponse = JSON.parse(text);
     return jsonResponse;
