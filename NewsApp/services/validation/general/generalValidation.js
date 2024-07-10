8ab7a90009b4148948f176d4ae510dd71dfa9de6
vico958 <vico958@gmail.com>
2024-07-10 16:46:17 +0300

    pass function from validation ai to general and remove folder of validation ai

--- NewsApp/services/validation/general/generalValidation.js
+++ NewsApp/services/validation/general/generalValidation.js
@@ -13,7 +13,21 @@ function isStringType(itemToCheckIfStringType){
         return true;
 }
 
+function isListAndFromStringType(listToCheck, listName) {
+    if (isArray(listToCheck) === false) {
+      return { valid: false, message: `${listName} should be an array.` };
+    }
+  
+    for (const item of listToCheck) {
+      if (isStringType(item) === false) {
+        return { valid: false, message: `All elements in the ${listName} should be strings.` };
+      }
+    }
+    return { valid: true, message: `${listName} are valid.` };
+}
+
 module.exports = {
     isStringType,
-    isArray
+    isArray,
+    isListAndFromStringType
   }
