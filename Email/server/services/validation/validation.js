620dec814aadf0fb0768e34e0302f6998b2a3022
vico958 <vico958@gmail.com>
2024-07-10 16:26:38 +0300

    add email validation to email service

---
+++ Email/server/services/validation/validation.js
@@ -0,0 +1,27 @@
+function isSendEmailInfoValid(emailHost, emailUser, emailPassword, emailFrom, emailTo, emailSubject, emailText){
+  if(isStringType(emailHost) === false || isEmailValidAsEmail(emailHost) === false) return false;
+  if(isStringType(emailUser) === false || isEmailValidAsEmail(emailUser) === false) return false;
+  if(isStringType(emailPassword) === false) return false;
+  if(isStringType(emailFrom) === false || isEmailValidAsEmail(emailFrom) === false) return false;
+  if(isStringType(emailTo) === false || isEmailValidAsEmail(emailTo) === false) return false;
+  if(isStringType(emailSubject) === false) return false;
+  if(isStringType(emailText) === false) return false;
+  return true;
+}
+
+function isEmailValidAsEmail(emailToCheck){
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(emailToCheck);
+}
+
+function isStringType(itemToCheckIfStringType){
+        if (typeof itemToCheckIfStringType !== 'string') {
+          return false;
+        }
+        return true;
+}
+
+
+module.exports = {
+  isSendEmailInfoValid
+}
