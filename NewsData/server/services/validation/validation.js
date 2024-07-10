c00ac026eac6981eadac03f49ef03c50b2113dff
vico958 <vico958@gmail.com>
2024-07-10 15:29:27 +0300

    add validation to news data

---
+++ NewsData/server/services/validation/validation.js
@@ -0,0 +1,32 @@
+function isListAndFromStringType(listToCheck, listName) {
+    if (isArray(listToCheck) === false) {
+      return { valid: false, message: `${listName} should be an array.` };
+    }
+  
+    for (const item of listToCheck) {
+      if (isStringType(item) === false) {
+        return { valid: false, message: `All elements in the ${listName} should be strings.` };
+      }
+    }
+    return { valid: true, message: `${listName} are valid.` };
+}
+  
+function isArray(arr){
+    if (!Array.isArray(arr)) {
+        return false;
+      }
+      return true;
+    
+}
+
+function isStringType(itemToCheckIfStringType){
+        if (typeof itemToCheckIfStringType !== 'string') {
+          return false;
+        }
+        return true;
+}
+
+
+module.exports = {
+    isListAndFromStringType
+}
