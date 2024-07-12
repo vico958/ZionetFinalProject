89450ddec99db4e240a4761eb99d2d432ea1ccfd
vico958 <vico958@gmail.com>
2024-07-13 02:31:01 +0300

    started the user accessor change to logger

--- UserAccessor/server.js
+++ UserAccessor/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
-const {logger} = require("./server/middleware/logger");
+const {loggerMiddleware} = require("./server/middleware/loggerMiddleware");
 const {errorHandler} = require("./server/middleware/errorHandler");
+const userAccessorLogger = require("./server/services/logger/logger");
 const bodyParser = require("body-parser");
 const userAccessor = require("./server/routes/userAccessor")
 const app = express();
-app.use(logger);
+app.use(loggerMiddleware);
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,10 +16,11 @@ app.use("/user-accessor", userAccessor)
 const port = process.env.port || "3003";
 
 app.get("/", (req, res) =>{
-    res.send("hello world user accessor service")
+    userAccessorLogger.info("Hello world from user accessor service")
+    res.send("Hello world from user accessor service")
 })
 app.use(errorHandler);
 
 app.listen(port, () =>{
-    console.log("Server started on port", port)
+    userAccessorLogger.info(`User accessor server started on port - ${port}`)
 })
--- UserAccessor/server/middleware/errorHandler.js
+++ UserAccessor/server/middleware/errorHandler.js
@@ -1,9 +1,18 @@
-
+const userAccessorLogger = require("../services/logger/logger");
 function errorHandler(error, req, res, next) {
-    if (res.headersSent) {
-        return next(error);
-    }
-    res.status(error.statusCode || 500).send(error.message || "Something went wrong from our side.");
+  const {statusCode, message} = error
+  userAccessorLogger.error({
+    error: message,
+  }, "Inside middleware error handler");
+  if (res.headersSent) {
+    userAccessorLogger.warn("Headers already sent. Passing the error to the next middleware.");
+    return next(error);
+  }
+  res.status(statusCode || 500).send(message || "Something went wrong from our side.");
+  userAccessorLogger.info({
+    status: statusCode, 
+    error_msg: message
+  }, "Error response sent");
 }
 
 module.exports = { errorHandler };
