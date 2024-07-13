7eaa8351f8bdaec20177600f7a0c5c7cbb29ef19
vico958 <vico958@gmail.com>
2024-07-13 21:29:14 +0300

    add logger to news data

--- NewsData/server/routes/newsData.js
+++ NewsData/server/routes/newsData.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {getNews, getCategoriesRules} = require("./newsDataFunctions");
+const {getNews, getCategoriesRules, hellowWorldCheck} = require("./newsDataFunctions");
 
 const newsDataRouter = express.Router();
 
 newsDataRouter.post("/get-news", getNews);
 newsDataRouter.get("/categories-rules", getCategoriesRules);
+newsDataRouter.get("/", hellowWorldCheck);
 
 module.exports = newsDataRouter;
