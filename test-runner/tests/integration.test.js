a814311ef2565894b74403dd41aa9eb79a87e75e
vico958 <vico958@gmail.com>
2024-07-14 14:29:37 +0300

    add Integration test for delete and Change Categories and Preferences

--- test-runner/tests/integration.test.js
+++ test-runner/tests/integration.test.js
@@ -38,7 +38,111 @@ describe('Integration Test - User Registration', () => {
 });
 
 
+describe('Integration Test - Change Categories and Preferences', () => {
+    it('Should change user categories and preferences and return the updated data', async () => {
+      const userWithNewSettings = {
+        email: 'test@example.com',
+        password: 'passworD123',
+        newCategories: ['science', 'technology'],
+        newPreferences: ['Science News', 'Tech Updates']
+      };
+  
+      const res = await chai.request('http://newsapp:3001')
+                            .put('/news/change-user-categories-and-preferences')
+                            .send({ userWithNewSettings });
+      const responseBody = jsonParseTextProperty(res);
+  
+      expect(res).to.have.status(200);
+      expect(responseBody).to.have.property('message').that.deep.equals("User preferences and categories have been updated.");
+      expect(responseBody).to.have.property('data').that.deep.include({
+        email: userWithNewSettings.email,
+        password: userWithNewSettings.password,
+        fullName: userWithNewSettings.fullName,
+        preferences: userWithNewSettings.newPreferences,
+        categories: userWithNewSettings.newCategories
+      });
+  
+      expect(responseBody.data).to.have.property('_id');
+      expect(responseBody.data).to.have.property('createdAt');
+      expect(responseBody.data).to.have.property('updatedAt');
+      expect(responseBody.data).to.have.property('__v');
+    });
+  });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe('Integration Test - User Deletion', () => {
+    it('Should delete an existing user and send a confirmation message', async () => {
+      const userToDelete = { email: 'test@example.com', password: 'passworD123' };
+  
+      const res = await chai.request('http://newsapp:3001')
+                            .delete('/news/delete-user')
+                            .send({ user: userToDelete });
+      const responseBody = jsonParseTextProperty(res);
+  
+      expect(res).to.have.status(200);
+      expect(responseBody).to.have.property('message').that.deep.equals('You have been removed from the news app.');
+      expect(responseBody).to.have.property('data').that.deep.include({
+        acknowledged: true,
+        deletedCount: 1
+      });
+    });
+  });
 
 function jsonParseTextProperty(res){
     const responseBody = JSON.parse(res.text);
